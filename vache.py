22fff1fccdc491c647299046d39dae9f8c842562
Seb770 <sebastien77.faurie77@gmail.com>
2023-12-21 14:16:32 +0100

    Vache qui donne son prenom

--- vache.py
+++ vache.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-print('coucou')
+class Vache:
+    def __init__(self, nom):
+        self.nom = nom
+
+    def parler(self):
+        return f"Moo ! Je m'appelle {self.nom} et je suis une vache heureuse."
+
+# Fonction principale
+def main():
+    # Créer une vache
+    ma_vache = Vache("Bella")
+
+    # Faire parler la vache
+    message = ma_vache.parler()
+
+    # Afficher le message
+    print(message)
+
+# Appeler la fonction principale
+if __name__ == "__main__":
+    main()
